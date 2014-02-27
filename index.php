a33b975fa815acef028e4e40fd76308296f9d963
emealia1 <emealia.hollis@ksuwdc.com>
2014-02-26 21:11:08 -0500

    Updates

--- index.php
+++ index.php
@@ -12,21 +12,24 @@
  * @subpackage 	Starkers
  * @since 		Starkers 4.0
  */
+
+
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
-<img src="logo.png">
+<section>
+<img id=logo src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo-ph.png">
 
-<nav>
-	<a href="/" class="nav-item">Page 1</a>
-	<a href="/" class="nav-item">Page 2</a>
-	<a href="/" class="nav-item">Page 3</a>
-	<a href="/" class="nav-item">Page 4</a>
+<nav class=float-right>
+	<a href="/" class="nav-item nav-item-active txt-center">Page 1</a>
+	<a href="/" class="nav-item txt-center">Page 2</a>
+	<a href="/" class="nav-item txt-center">Page 3</a>
+	<a href="/" class="nav-item txt-center">Page 4</a>
 </nav>
 
 <div class="main-img"></div>
 
-<section>
+<section class=wdth-full>
 	<p class="col">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quam erat, lacinia nec semper et, condimentum quis ipsum. Morbi lacinia vitae sem ac pretium. In tempor, ligula sit amet volutpat cursus, quam magna rutrum mauris, vitae tristique elit turpis ut felis.
 	</p>
 
@@ -35,24 +38,7 @@
 
 	<p class="col">Sed rhoncus faucibus felis ac sagittis. Sed semper cursus semper. Nullam vitae mauris eu lacus convallis ultrices. Curabitur lobortis velit a libero suscipit, tincidunt sodales diam auctor. Nam tincidunt turpis ac pellentesque faucibus.
 	</p>
-</section>	
-
-
-<?php if ( have_posts() ): ?>
-<h2>Latest Posts</h2>	
-<ol>
-<?php while ( have_posts() ) : the_post(); ?>
-	<li>
-		<article>
-			<h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-			<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
-			<?php the_content(); ?>
-		</article>
-	</li>
-<?php endwhile; ?>
-</ol>
-<?php else: ?>
-<h2>No posts to display</h2>
-<?php endif; ?>
+</section>
+</section>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer') ); ?>
--- parts/shared/footer.php
+++ parts/shared/footer.php
@@ -1,7 +1,8 @@
 	
 	<footer>
+		<?php get_search_form(); ?>
 		<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/FB-f-Logo__blue_72.png" alt="facebook"> Facebook
 		<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Twitter_logo_white_72.png" alt="twitter"> Twitter
 		<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/IG.png" alt="instagram"> Instagram
-		&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.
+		<div class="clear-both txt-center">&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.</div>
 	</footer>
