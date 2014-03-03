f3ffc3b513b11fed445bfe549341dd2570513162
emealia1 <emealiahollis1@Emealias-Mac-mini.local>
2014-03-03 06:50:13 -0500

    Updates

--- index.php
+++ index.php
@@ -17,28 +17,34 @@
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
-<section>
-<img id=logo src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo-ph.png">
+<section id="hght-20" class="clear-both">
+	<img id="logo" src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo-ph.png">
 
-<nav class=float-right>
-	<a href="/" class="nav-item nav-item-active txt-center">Page 1</a>
-	<a href="/" class="nav-item txt-center">Page 2</a>
-	<a href="/" class="nav-item txt-center">Page 3</a>
-	<a href="/" class="nav-item txt-center">Page 4</a>
-</nav>
+	<nav class="float-right txt-white">
+		<a href="/" class="nav-item nav-item-active txt-center">Page 1</a>
+		<a href="/" class="nav-item txt-center">Page 2</a>
+		<a href="/" class="nav-item txt-center">Page 3</a>
+		<a href="/" class="nav-item txt-center">Page 4</a>
+	</nav>
+</section>
 
-<div class="main-img"></div>
+<section id="hght-40" class="main-img">
+	<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Beach-Landscape.png">
+</section>
 
-<section class=wdth-full>
-	<p class="col">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quam erat, lacinia nec semper et, condimentum quis ipsum. Morbi lacinia vitae sem ac pretium. In tempor, ligula sit amet volutpat cursus, quam magna rutrum mauris, vitae tristique elit turpis ut felis.
+<section id="hght-30">
+	<p class="col">
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quam erat, lacinia nec semper et, condimentum quis ipsum. Morbi lacinia vitae sem ac pretium. In tempor, ligula sit amet volutpat cursus, quam magna rutrum mauris, vitae tristique elit turpis ut felis.
 	</p>
 
-	<p class="col">Integer augue metus, molestie ac varius quis, tincidunt sed neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas aliquam sed massa eu condimentum. Sed aliquet odio quis varius vulputate. 
+	<p class="col">
+	Integer augue metus, molestie ac varius quis, tincidunt sed neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas aliquam sed massa eu condimentum. Sed aliquet odio quis varius vulputate. 
 	</p>
 
-	<p class="col">Sed rhoncus faucibus felis ac sagittis. Sed semper cursus semper. Nullam vitae mauris eu lacus convallis ultrices. Curabitur lobortis velit a libero suscipit, tincidunt sodales diam auctor. Nam tincidunt turpis ac pellentesque faucibus.
+	<p class="col">
+	Sed rhoncus faucibus felis ac sagittis. Sed semper cursus semper. Nullam vitae mauris eu lacus convallis ultrices. Curabitur lobortis velit a libero suscipit, tincidunt sodales diam auctor. Nam tincidunt turpis ac pellentesque faucibus.
 	</p>
 </section>
-</section>
+
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer') ); ?>
--- parts/shared/footer.php
+++ parts/shared/footer.php
@@ -1,8 +1,10 @@
 	
-	<footer>
-		<?php get_search_form(); ?>
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/FB-f-Logo__blue_72.png" alt="facebook"> Facebook
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Twitter_logo_white_72.png" alt="twitter"> Twitter
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/IG.png" alt="instagram"> Instagram
-		<div class="clear-both txt-center">&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.</div>
+	<footer id="hght-10" class="txt-white wdth-full">
+		<div class="txt-center">
+			<?php get_search_form(); ?>
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/FB-f-Logo__blue.png" alt="facebook"> Facebook
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Twitter_logo_white.png" alt="twitter"> Twitter
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/IG.png" alt="instagram"> Instagram
+			<div class="clear-both">&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.</div>
+		</div>
 	</footer>
